76903a4b2852b520876b1eb1dd2bcd6b193fda6d
Codeandomx <admin@codeando.org>
2018-01-15 16:16:40 -0600

    Ejemplo Hola mundo.

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const rootElement = document.getElementById('root');
-let name = 'Codeando';
-
-const element = (
-    <div className="main">
-        <h1>Hola!</h1>
-        <h2>Bienvenidos a {name}</h2>
-		    <img src="logo.png" />
-    </div>
-);
-
 ReactDOM.render(
-    element,
-    rootElement
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
 );
